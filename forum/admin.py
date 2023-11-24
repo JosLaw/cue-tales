10b55e7119b870f0bb03ab3d60be95593c8e1d00
JosLaw <>
2023-11-24 18:40:39 +0000

    Add classes to models.py and admin.py files

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Cue, Post, Review
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+class CueAdmin(admin.ModelAdmin):
+    list_display = ("prompt", "date")
+
+
+admin.site.register(Cue, CueAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author")
+
+
+admin.site.register(Post, PostAdmin)
--- forum/models.py
+++ forum/models.py
@@ -5,13 +5,37 @@ from cloudinary.models import CloudinaryField  # images
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
+class Cue(models.Model):
+    prompt = models.CharField(max_length=45, unique=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.prompt
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150, unique=True)
     slug = models.SlugField(max_length=150, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts")
-    posted_on = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="forum_posts")
+    published = models.DateField(auto_now_add=True)
     edited_on = models.DateField(auto_now=True)
     content = models.TextField
-    preview = models.TextField(blank=True)
+    plot = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name="forum_likes", blank=True)
+    likes = models.ManyToManyField(
+        User, related_name="post_likes", blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_comment")
+    posted_on = models.DateTimeField(auto_now_add=True)
+    body = models.CharField(max_length=280)  # old twitter limit
+    approve = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Review {self.body} by {self.name} "
